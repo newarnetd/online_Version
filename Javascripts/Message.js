8fc659a93dd3438a290183bb5e6e38c7eecb76a5
newarnetd <newarnetdevs@gmail.com>
2023-12-25 13:27:08 +0200

    new

--- Javascripts/Message.js
+++ Javascripts/Message.js
@@ -2,3 +2,34 @@
  {
     document.querySelector(".iconsHeader.iconSearch").style.display='none';
  }
+
+ function ActionUser(event) {
+   var checkbox = event.target;
+
+   let mere = event.target.closest('.swiper-slide.GestionMembre');
+   let inputCheckboxes = mere.querySelectorAll("input[type='checkbox']");
+   
+   let shouldDisplay = Array.from(inputCheckboxes).some(input => input.checked);
+
+   if (shouldDisplay) {
+       document.querySelector(".swiper-slide.GestionMembre .InputDouble").style.display = 'flex';
+   } else {
+       document.querySelector(".swiper-slide.GestionMembre .InputDouble").style.display = 'none';
+   }
+}
+ function Actionblocage(event) {
+   var checkbox = event.target;
+
+   let mere = event.target.closest('.swiper-slide.Blocage');
+   let inputCheckboxes = mere.querySelectorAll("input[type='checkbox']");
+   
+   let shouldDisplay = Array.from(inputCheckboxes).some(input => input.checked);
+
+   if (shouldDisplay) {
+       document.querySelector(".swiper-slide.Blocage .InputDouble").style.display = 'flex';
+   } else {
+       document.querySelector(".swiper-slide.Blocage .InputDouble").style.display = 'none';
+   }
+}
+
+
--- Javascripts/MessageStructure.js
+++ Javascripts/MessageStructure.js
@@ -2,7 +2,7 @@ const menuSwipper = document.querySelector(".swiper-wrapper.LeftSide");
 const menuSwipperDroit = document.querySelector(".swiper-wrapper.DroiteSide");
 const leftIcon = document.querySelector(".iconsHeader.Returnback");
 const leftIconSearch = document.querySelector(".iconsHeader.search");
-function MenuFils()
+function Filemenu()
 {
     menuSwipperDroit.style.transform = "TranslateX(-100%)";
 }
@@ -36,12 +36,31 @@ function OptionMore()
     leftIcon.style.display = 'flex';
     leftIconSearch.style.display = 'none';
 }
+function GestionMembresDuGroupe()
+{
+    menuSwipper.style.transform = "TranslateX(-500%)";
+}
+function BloquerUser()
+{
+    menuSwipper.style.transform = "TranslateX(-600%)";
+}
+function ConditiondUsage()
+{
+    menuSwipper.style.transform = "TranslateX(-700%)";
+}
 function ChampVerifiction(event)
 {
-    if(event.target.vulue != '')
+    if(event.target.value.trim() !== '')
     {
         document.querySelector(".messagecontent .iconsDroite .sendsMessage").style.display= 'flex';
+        document.querySelector("p.EncoursAction").textContent = "Ecris...";
+        document.querySelector("p.EncoursAction").style.opacity = 1;
+        document.querySelector("p.EncoursAction").style.color ='var(--color-primary)';
+
     }else{
-        document.querySelector(".messagecontent .iconsDroite .sendsMessage").style.display= 'flex';
+        document.querySelector(".messagecontent .iconsDroite .sendsMessage").style.display= 'none';
+        document.querySelector("p.EncoursAction").textContent = "En ligne";
+        document.querySelector("p.EncoursAction").style.opacity = .6;
+        document.querySelector("p.EncoursAction").style.color ='var(--color-text)';
     }
 }
